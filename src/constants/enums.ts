2fec24c753b06b012f356f1f1911bd13633c327e
anveio <im.shovonhasan@gmail.com>
2018-07-09 06:59:12 -0400

    Use GoogleScope enum for scope.

---
+++ src/constants/enums.ts
@@ -0,0 +1,3 @@
+export enum GoogleScope {
+  ANALYTICS_READONLY = 'https://www.googleapis.com/auth/analytics.readonly'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { google } from 'googleapis';
+import { GoogleScope } from './constants/enums';
 // tslint:disable:no-console
 
 async function main() {
   console.log('Authorizing client...');
   const auth = await google.auth.getClient({
     keyFile: __dirname + '/gcloud_api_key.json',
-    // Scopes can be specified either as an array or as a single, space-delimited string.
-    scopes: ['https://www.googleapis.com/auth/compute']
+    scopes: GoogleScope.ANALYTICS_READONLY
   });
 
   const analytics = google.analytics({
@@ -16,7 +16,7 @@ async function main() {
 
   console.log('Client authorized!');
 
-  const analyticsData = await analytics.data.realtime;
+  const analyticsData = analytics.data.realtime;
 
   console.log(analyticsData);
 }

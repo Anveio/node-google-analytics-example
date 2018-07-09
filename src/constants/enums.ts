4f304e41cf002fca8bc73aeebbb060028a28a4ee
anveio <im.shovonhasan@gmail.com>
2018-07-09 07:17:45 -0400

    Include sample query.

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -1,3 +1,7 @@
 export enum GoogleScope {
   ANALYTICS_READONLY = 'https://www.googleapis.com/auth/analytics.readonly'
 }
+
+export enum Metrics {
+  ACTIVE_USERS = 'rt:activeUsers'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
 import { google } from 'googleapis';
-import { GoogleScope } from './constants/enums';
+import { GoogleScope, Metrics } from './constants/enums';
 // tslint:disable:no-console
 
 async function main() {
   console.log('Authorizing client...');
+
   const auth = await google.auth.getClient({
     keyFile: __dirname + '/gcloud_api_key.json',
     scopes: GoogleScope.ANALYTICS_READONLY
   });
 
+  console.log('Client authorized!');
+
+  // The `analytics` object will now send along authorization with each query.
   const analytics = google.analytics({
     version: 'v3',
     auth
   });
 
-  console.log('Client authorized!');
+  /**
+   * See https://developers.google.com/apis-explorer/#p/analytics/v3/analytics.data.realtime.get
+   */
+  const { data } = await analytics.data.realtime.get({
+    // The profile ID is found under Dashboard > Admin > View Settings
+    ids: 'ga:178185000',
+    metrics: Metrics.ACTIVE_USERS
+  });
 
-  const analyticsData = analytics.data.realtime;
-
-  console.log(analyticsData);
+  console.log(data);
 }
 
 main().catch(console.error);
